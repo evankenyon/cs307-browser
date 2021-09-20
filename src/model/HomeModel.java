e1622e22fc6ea749191ec26d5f76e80e9beaed94
Evan Kenyon <ek168@duke.edu>
2021-09-20 15:36:24 -0400

    Added documentation for HomeModel

--- src/model/HomeModel.java
+++ src/model/HomeModel.java
@@ -3,11 +3,11 @@ package model;
 import java.net.URL;
 
 /**
- * Purpose:
- * Assumptions:
- * Dependencies:
- * Example:
- * Other details:
+ * Purpose: Represent a model component that saves/sets a homeURL,
+ * and returns it when the user wants to go to that URL
+ * Dependencies: URL
+ * Example: Construct a HomeModel object in NanoBrowserDisplay in order
+ * to handle the logic for when set and get home buttons are pressed
  *
  * @Author Evan Kenyon
  */
@@ -15,21 +15,28 @@ public class HomeModel {
     private URL homeURL;
 
     /**
-     * Purpose:
-     * Assumptions:
-     * @param currURL
+     * Purpose: Construct a HomeModel object with homeURL = null
+     */
+    public HomeModel() {
+        homeURL = null;
+    }
+
+    /**
+     * Purpose: Set the home URL to currURL
+     * Assumptions: currURL is the current URL that
+     * the user is on in the browser
+     * @param currURL the URL to set homeURL to
      */
     public void setHome(URL currURL) {
         homeURL = currURL;
     }
 
     /**
-     * Purpose:
-     * Assumptions:
-     * @return
-     * @throws NullPointerException
+     * Purpose: Get the home URL
+     * @return the home URL
+     * @throws NullPointerException if homeURL was not set before this function
+     * is called
      */
-    // TODO: Change exception type?
     public URL getHome() throws NullPointerException {
         if(homeURL == null) {
             throw new NullPointerException();
